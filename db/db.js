933f041a138ce90622f9d085ef105e1ee3c8173d
Ryan Seaman <ryan.l.seaman@gmail.com>
2025-02-03 19:26:41 -0600

    added world clock stuff

--- db/db.js
+++ db/db.js
@@ -1,3 +1,6 @@
+const zoneinfo = require('zoneinfo')
+const clock = require('world-clock')()
+
 class Storage {
     constructor() {
         this.storage = [
@@ -13,17 +16,38 @@ class Storage {
             }
         ];
 
+        this.zones = zoneinfo.listTimezones()
       }
 
       addMessage(text, user) {
-        const date = new Date()
-        this.storage.push({ text, user, date });
+        const timezone = this.getCountry(); 
+        const localTime = clock.localDateTime(timezone);
+    
+        const date = new Date(localTime);
+        
+        const formattedDate = new Intl.DateTimeFormat('en-US', {
+            timeZone: timezone,
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            timeZoneName: 'short'
+        }).format(date);
+    
+        this.storage.push({ text, user, added: formattedDate }); 
         console.log(this.storage);
-      }
+    }
+    
     
       getMessages() {
         return this.storage;
       }
+
+    getCountry() {
+        return this.zones[Math.floor(Math.random() * this.zones.length)]; 
+    }
 }
 
 module.exports = new Storage(); 
